be974efdb52998802698349974b669e14f660971
Justin Cs <138377459+thejzxt@users.noreply.github.com>
2026-05-31 21:36:20 +0530

    Hide Firebase keys using env variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCeSokhWEsOZVnC8cg3q-PcbG5fk9hSOs8",
-  authDomain: "smart-incentitive-calculator.firebaseapp.com",
-  projectId: "smart-incentitive-calculator",
-  storageBucket: "smart-incentitive-calculator.firebasestorage.app",
-  messagingSenderId: "1058180822201",
-  appId: "1:1058180822201:web:fe39c02f2ecbd50e0cd768",
-  measurementId: "G-QVW2PNXCQ6"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
